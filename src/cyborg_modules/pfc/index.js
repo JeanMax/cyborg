ad2ca7e4ee02e815318ddc8db0891bf66f03cb3a
zougli <guillaume.gence@gmail.com>
2017-05-29 21:15:30 +0200

    fin pfc

--- src/cyborg_modules/pfc/index.js
+++ src/cyborg_modules/pfc/index.js
@@ -10,8 +10,14 @@ const events = require('events');
 const connect = require('connect');
 const pfc = require('./lib/pfc.js');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
 
 var eventEmitter = new events.EventEmitter();
+
+var mySession;
+
+app.use(session({secret: 'secret2'}));
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -21,27 +27,46 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get('/', function (req, res) {
-  idJoueur += 1;
-  res.render('menu', { idJoueur: idJoueur });
+  mySession = req.session;
+
+  if(mySession.idJoueur){
+      var id = mySession.idJoueur;
+      res.render('menu', { idJoueur: id });
+  }else {
+    if(idJoueur >= nbJoueursMax){
+      res.send('deso')
+    }else{
+      mySession.idJoueur = ++idJoueur;
+      var id = mySession.idJoueur;
+      res.render('menu', { idJoueur: id });
+    }
+  }
 })
 
-app.post('/choix', function (req, res) {
+function hasIdJoueur(req,res,next) {
+  if(req.session){
+    next();
+  }else {
+    res.send("DESO, je t'ai dis !!!");
+  }
+}
+
+app.post('/choix',hasIdJoueur, function (req, res) {
   var idJoueur = req.body.idJoueur;
   var choix = req.body.choix;
-  pfc.set(idJoueur,choix);
+  if(!pfc.isOk()){
+      pfc.set(idJoueur,choix);
+  }
   res.render('result');
 })
 
-app.get('/result', function (req, res) {
-
+app.get('/result',hasIdJoueur, function (req, res) {
 
   res.writeHead(200, {
      'Connection': 'keep-alive',
      'Content-Type': 'text/event-stream',
      'Cache-Control': 'no-cache'
    });
-
-
 
    eventEmitter.on('result', function () {
        var testdata = pfc.result();
@@ -52,6 +77,10 @@ app.get('/result', function (req, res) {
      eventEmitter.emit('result');
    }
 
+})
+
+app.get('/resultFin', function (req, res) {
+  res.send("Le gagnant est "+ pfc.result() );
 })
 
 app.listen(port, function () {
